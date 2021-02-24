61835c2cbfb5dd7496966802eefcedc5f60b9a5b
sk534 <asatish764@gmail.com>
2021-02-24 18:07:28 +0530

    2nd commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+require("bootstrap")
+require("jquery")
+require("@nathanvda/cocoon")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+import "../stylesheets/application";
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
